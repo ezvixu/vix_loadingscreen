ec99dd58fc1be4b1d18793ebd51696bc19987a27
ezvixu <161530660+ezvixu@users.noreply.github.com>
2024-02-29 00:41:53 +0100

    Add files via upload

---
+++ vix_loadingscreen/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'vix'
+description 'loading screen'
+version '1.0.0'
+
+loadscreen 'ui/main.html'
+
+files {
+    'ui/main.html',
+    'ui/css/main.css',
+    'ui/img/*.png',
+    'ui/audio.mp3'
+}
